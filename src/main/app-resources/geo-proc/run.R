2e5c02d8e679744fd6d49e69545d7dfe36751478
Costanzo <costanzo.d@gmail.com>
2014-10-17 14:25:48 +0200

    typos
    fixing some typos

--- src/main/app-resources/geo-proc/run.R
+++ src/main/app-resources/geo-proc/run.R
@@ -37,7 +37,7 @@ while(length(ls8.ref <- readLines(f, n=1)) > 0) {
 	ls8.identifier <- strsplit(rciop.casmeta(field="dc:identifier", url=ls8.ref)$output, ":")[[1]][2]
 
 	# download Landsat 8 product
-	rciop.log("INFO", paste0("downloading", ls8.url, "to", ls8.identifier))
+	rciop.log("INFO", paste("downloading", ls8.url, "to", ls8.identifier))
 	rciop.copy (url=ls8.downloadUrl, target=TMPDIR, uncompress=FALSE)
 
 	# extract the compressed Landsat 8 product
@@ -45,57 +45,36 @@ while(length(ls8.ref <- readLines(f, n=1)) > 0) {
 	untar(paste(TMPDIR, "/", ls8.identifier ,".tar.gz", sep=""), exdir = ls8.identifier)
 
 	# read the data
-	rciop.log("INFO", paste0("Loading", ls8.identifier, "dataset"))
+	rciop.log("INFO", paste("Loading", ls8.identifier, "dataset"))
 	ls8 <- ReadLandsat8(ls8.identifier, aoi.extent)
 
 	r <- raster()
+	coverage.name <- ""
 	if (GetOrbitDirection(ls8) == 'A') {
-                rciop.log("INFO", "Ascending orbit, saving TIRS1 band")
                 # ascending direction, get AtSatelliteBrightnessTemperature from TIRS1 
                 bt <- ToAtSatelliteBrightnessTemperature(ls8, band="tirs1")
-                coverage.store <- paste("Thermal", ls8.identifier, sep="_")               
+                coverage.name <- "Thermal"
     } else {
-                rciop.log("INFO", "Descending orbit, saving RGB image")
                 # descending direction, get RGB from "swir2", "nir", "green" bands
                 raster.image <- ToRGB(ls8, "swir2", "nir", "green")
-                coverage.store <- paste("Colours", ls8.identifier, sep="_")    
+                coverage.name <- "Colours"
     }
-
-    tryCatch({r <- projectRaster(bt, crs=dest.proj)},
-                	error = function(err){
-                		rciop.log("INFO",paste("projectRaster error:",err))
-                })
+	
+	coverage.store <- paste(coverage.name, ls8.identifier, sep="_")               
+	rciop.log("INFO", paste0("coverage.store = ",coverage.store))   
+    r <- projectRaster(bt, crs=dest.proj)
     
-
-
-    rciop.log("INFO", paste0("coverage.store = ",coverage.store))   
     
-    tryCatch({
-    			CreateGeoServerCoverageStore(geoserver,
-                			                    workspace.name,
-                            			        coverage.store,
-			                                    TRUE,
-			                                    "GeoTIFF",
-			                                    "file:data/raster.tif")},
-                	error = function(err){
-                		rciop.log("INFO",paste("CreateGeoServerCoverageStore error:",err))
-            })
+    
+	CreateGeoServerCoverageStore(geoserver,
+			                    workspace.name,
+            			        coverage.store,
+                                TRUE,
+                                "GeoTIFF",
+                                "file:data/raster.tif")
     
     rciop.log("INFO", "Executing POSTRaster")
-    tryCatch({POSTraster(geoserver, workspace.name, coverage.store, r)},
-                	error = function(err){
-                		rciop.log("INFO",paste("POSTraster error:",err))
-            })
-    
-
-
-	# publish it
-	# res <- rciop.publish(ls8.png, recursive=FALSE, metalink=TRUE)
-	# if (res$exit.code==0) { published <- res$output }
-
-	# publish it
-	# res <- rciop.publish(ls8.tif, recursive=FALSE, metalink=TRUE)
-	# if (res$exit.code==0) { published <- res$output }
+    POSTraster(geoserver, workspace.name, coverage.store, r)
 
 	# clean up
 	rciop.log("INFO", "Cleaning-up")
